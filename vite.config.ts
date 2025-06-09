219b5093b34bc9c2c391b91ab1c8cfdcffb2e445
GalinaKryvoboka <galinakryvoboka@gmail.com>
2025-06-10 02:51:30 +0300

    Fix base config for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/project-bolt-clean/", // 🔁 заміни на назву свого GitHub репозиторію!
+  base:
+    process.env.VERCEL === "1"
+      ? "/" // для Vercel
+      : "/korenovska/", // для GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
