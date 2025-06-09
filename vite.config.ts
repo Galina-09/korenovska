b0ecb9a0bc876f7a246d0707651e09877f2ca478
GalinaKryvoboka <galinakryvoboka@gmail.com>
2025-06-10 02:23:48 +0300

    new1

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/project-bolt-clean/", // 🔁 заміни на назву свого GitHub репозиторію!
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
